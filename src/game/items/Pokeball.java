c60a7780f45ceab40de09f6f598f9a17b94016e6
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-13 17:33:52 +0800

    Pokeball class created and implemenation provided.

---
+++ src/game/items/Pokeball.java
@@ -0,0 +1,17 @@
+package game.items;
+
+import edu.monash.fit2099.engine.items.Item;
+import game.pokemons.Pokemon;
+
+public class Pokeball extends Item {
+
+    Pokemon caughtPokemon;
+
+    public Pokeball() {
+        super("Pokeball", '0', false);
+    }
+
+    public void assignCaughtPokemon(Pokemon caughtPokemon) {
+        this.caughtPokemon = caughtPokemon;
+    }
+}
