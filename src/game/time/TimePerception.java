db6dde971473844a985be558466d917a3f94e461
Your Name Here <eooi0006@student.monash.edu>
2022-09-17 20:05:50 +0800

    Java doc added for the time perception interface

--- src/game/time/TimePerception.java
+++ src/game/time/TimePerception.java
@@ -3,29 +3,23 @@ package game.time;
 /**
  * Created by:
  * @author Riordan D. Alfredo
- * Modified by:
- *
+ * Modified by: Eng Lim Ooi
+ * An interface for classes with time perception to implement.
  */
 public interface TimePerception {
     /**
-     * TODO: override this method, and execute this method inside the relevant manager.
+     * The dayEffect is implemented in the classes that use the TimePerception interface.
      */
     void dayEffect();
 
     /**
-     * TODO: override this method, and execute this method inside the relevant manager.
+     * The nightEffect is implemented in the classes that use the TimePerception interface.
      */
     void nightEffect();
 
     /**
      * a default interface method that register current instance to the Singleton manager.
-     * It allows corresponding class uses to be affected by global reset
-     * TODO: Use this method at the constructor of the concrete class that implements it (`this` instance).
-     *       For example:
-     *       Simple(){
-     *          // other stuff for constructors.
-     *          this.registerInstance()  // add this instance to the relevant manager.
-     *       }
+     * It allows corresponding class uses to be affected by global reset.
      */
     default void registerInstance(){
         TimePerceptionManager.getInstance().append(this);
