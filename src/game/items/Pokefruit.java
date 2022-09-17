9c76170b26a04aa801d051bdbe65acc561316427
Your Name Here <eooi0006@student.monash.edu>
2022-09-17 17:28:35 +0800

    Java doc added for the Pokefruit class

--- src/game/items/Pokefruit.java
+++ src/game/items/Pokefruit.java
@@ -4,9 +4,19 @@ import edu.monash.fit2099.engine.items.Item;
 import game.elements.Element;
 import game.nusre.Tradeable;
 
+/**
+ * A class that represents Pokefruit item.
+ */
 public class Pokefruit extends Item implements Tradeable {
+    /**
+     * The element of the Pokefruit.
+     */
     private Element pokefruitElement;
 
+    /**
+     * Pokefruit Constructor.
+     * @param pokefruitElement element of the Pokefruit.
+     */
     public Pokefruit(Element pokefruitElement) {
         super(pokefruitElement + " Pokefruit", 'f', true);
         this.pokefruitElement = pokefruitElement;
@@ -14,6 +24,10 @@ public class Pokefruit extends Item implements Tradeable {
 
     }
 
+    /**
+     * To identify if this Tradeable is a Pokemon.
+     * @return boolean false.
+     */
     @Override
     public boolean isPokemon() {
         return false;
