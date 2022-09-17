30e01e76c3463004479c86a8d4041d536f4859a2
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-17 22:17:34 +0800

    day and night effect implemented for Puddle class

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -1,10 +1,19 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
 import game.elements.Element;
+import game.elements.ElementsHelper;
 import game.time.TimePerception;
+import game.time.TimePerceptionManager;
 
 public class Puddle extends Ground implements TimePerception {
+
+    // Location of the ground object on the map
+    Location location;
+
     /**
      * Constructor.
      *
@@ -16,12 +25,28 @@ public class Puddle extends Ground implements TimePerception {
     }
 
     @Override
-    public void dayEffect() {
+    public void tick(Location location) {
+        this.location = location;
+    }
 
+    @Override
+    public void dayEffect() {
+        if (Math.random() <= 0.1 && this.location != null){
+            this.location.setGround(new Dirt());
+            TimePerceptionManager.getInstance().cleanUp(this);
+        }
     }
 
     @Override
     public void nightEffect() {
-
+        if (Math.random() <= 0.1 && this.location != null) {
+            for(Exit exit : this.location.getExits()) {
+                Ground surroundingGround = exit.getDestination().getGround();
+                if (!ElementsHelper.hasAnySimilarElements(surroundingGround, this.findCapabilitiesByType(Element.class)) && !surroundingGround.hasCapability(Status.CANNOT_BE_EXPANDED)) {
+                    exit.getDestination().setGround(new Puddle());
+                }
+            }
+        }
     }
 }
+
