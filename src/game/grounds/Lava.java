d2dd293fee6b6ef8e7c255f681d6aba865e5b161
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-17 22:18:39 +0800

    day and night effect implemented for Lava class

--- src/game/grounds/Lava.java
+++ src/game/grounds/Lava.java
@@ -1,8 +1,13 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
 import game.elements.Element;
+import game.elements.ElementsHelper;
 import game.time.TimePerception;
+import game.time.TimePerceptionManager;
 
 /**
  * Created by:
@@ -11,6 +16,10 @@ import game.time.TimePerception;
  *
  */
 public class Lava extends Ground implements TimePerception {
+
+    // Location of the ground object on the map
+    Location location;
+
     /**
      * Constructor.
      */
@@ -21,13 +30,27 @@ public class Lava extends Ground implements TimePerception {
     }
 
     @Override
-    public void dayEffect() {
+    public void tick(Location location) {
+        this.location = location;
+    }
 
+    @Override
+    public void dayEffect() {
+        if (Math.random() <= 0.1 && this.location != null) {
+            for(Exit exit : this.location.getExits()) {
+                Ground surroundingGround = exit.getDestination().getGround();
+                if (!ElementsHelper.hasAnySimilarElements(surroundingGround, this.findCapabilitiesByType(Element.class)) && !surroundingGround.hasCapability(Status.CANNOT_BE_EXPANDED)) {
+                    exit.getDestination().setGround(new Lava());
+                }
+            }
+        }
     }
 
     @Override
     public void nightEffect() {
-//        if (Math.random() <= 0.1 && this.)
-
+        if ((Math.random() <= 0.1 && this.location != null) && !location.containsAnActor()){
+            this.location.setGround(new Dirt());
+            TimePerceptionManager.getInstance().cleanUp(this);
+        }
     }
 }
