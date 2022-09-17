32c5dca1e0ada2e98070e3007f3fa90c65a63ccf
Your Name Here <eooi0006@student.monash.edu>
2022-09-17 17:14:30 +0800

    Java doc added for the Pokeball class

--- src/game/items/Pokeball.java
+++ src/game/items/Pokeball.java
@@ -3,18 +3,34 @@ package game.items;
 import edu.monash.fit2099.engine.items.Item;
 import game.pokemons.Pokemon;
 
+/**
+ * @author Eng Lim Ooi
+ * A class representing the Pokeball item.
+ */
 public class Pokeball extends Item {
-
+    /**
+     * The captured Pokemon.
+     */
     Pokemon caughtPokemon;
 
+    /**
+     * Pokeball Constructor.
+     */
     public Pokeball() {
         super("Pokeball", '0', false);
     }
 
+    /**
+     * Assign a Pokemon to the Pokeball when the Pokemon is successfully captured.
+     * @param caughtPokemon Pokemon that has been caught
+     */
     public void assignCaughtPokemon(Pokemon caughtPokemon) {
         this.caughtPokemon = caughtPokemon;
     }
 
+    /**
+     * Returns the Pokemon that has been caught.
+     */
     public Pokemon getCaughtPokemon() {
         return caughtPokemon;
     }
