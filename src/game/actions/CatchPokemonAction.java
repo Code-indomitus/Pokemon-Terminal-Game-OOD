aa872febb3da17879f4c0afbec2c9681f5741f6e
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-18 23:29:11 +0800

    implemented CatchPokemonAction to catch an adjacent pokemon by the player

--- src/game/actions/CatchPokemonAction.java
+++ src/game/actions/CatchPokemonAction.java
@@ -1,4 +1,37 @@
 package game.actions;
 
-public class CatchPokemonAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.items.Pokeball;
+import game.pokemons.AffectionManager;
+import game.pokemons.Pokemon;
+
+public class CatchPokemonAction extends Action {
+
+    Pokemon pokemonToBeCaught;
+
+    public CatchPokemonAction(Pokemon pokemonToBeCaught) {
+        this.pokemonToBeCaught = pokemonToBeCaught;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        if (AffectionManager.getInstance().getAffectionPoint(this.pokemonToBeCaught) < 50 ) {
+            AffectionManager.getInstance().decreaseAffection(this.pokemonToBeCaught, 10);
+            return pokemonToBeCaught + " could not be captured.";
+        }
+
+        Pokeball newPokeball = new Pokeball();
+        newPokeball.assignCaughtPokemon(pokemonToBeCaught);
+        actor.addItemToInventory(newPokeball);
+        map.removeActor(pokemonToBeCaught);
+
+        return pokemonToBeCaught + " has been successfully captured.";
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "Catch " + pokemonToBeCaught + ".";
+    }
 }
