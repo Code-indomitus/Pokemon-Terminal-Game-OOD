9d0171cd5893d535439e48c3765183846fc3ab01
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-18 23:25:06 +0800

    implementation of Ember completed

--- src/game/weapons/Ember.java
+++ src/game/weapons/Ember.java
@@ -1,5 +1,20 @@
 package game.weapons;
 
-public class Ember {
+import edu.monash.fit2099.engine.weapons.WeaponItem;
 
+public class Ember extends WeaponItem {
+
+
+    /**
+     * Constructor.
+     *
+     * @param name        name of the item
+     * @param displayChar character to use for display when item is on the ground
+     * @param damage      amount of damage this weapon does
+     * @param verb        verb to use for this weapon, e.g. "hits", "zaps"
+     * @param hitRate     the probability/chance to hit the target.
+     */
+    public Ember(String name, char displayChar, int damage, String verb, int hitRate) {
+        super(name, displayChar, damage, verb, hitRate);
+    }
 }
