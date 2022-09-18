c34e24f3d29ac0e908cf64be9cbcd6a1af596697
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-18 21:47:43 +0800

    refactored changes to Pokemon class to abstract behaviours

--- src/game/pokemons/Pokemon.java
+++ src/game/pokemons/Pokemon.java
@@ -1,12 +1,46 @@
 package game.pokemons;
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
 import game.Status;
+import game.behaviours.AttackBehaviour;
+import game.behaviours.Behaviour;
+import game.behaviours.FollowBehaviour;
+import game.behaviours.WanderBehaviour;
+
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public abstract class Pokemon extends Actor {
-    private final int MAX_POKEMON_HP = 100;
+    private final Map<Integer, Behaviour> behaviours = new TreeMap<>(); // priority, behaviour
+
+
     public Pokemon(String pokemonName, char pokemonDisplayChar) {
         super(pokemonName, pokemonDisplayChar, 100);
         this.addCapability(Status.CANNOT_ENTER_FLOOR);
+        AffectionManager.getInstance().registerPokemon(this);
+
+        this.behaviours.put(10, new WanderBehaviour());
+        this.behaviours.put(20, new FollowBehaviour(AffectionManager.getInstance().getTrainer()));
+        this.behaviours.put(30, new AttackBehaviour());
+
     }
+
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        for (Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.getAction(this, map);
+            if (action != null)
+                return action;
+        }
+        return new DoNothingAction();
+    }
+
+
+
+//    public abstract void addWeapon();
 }
