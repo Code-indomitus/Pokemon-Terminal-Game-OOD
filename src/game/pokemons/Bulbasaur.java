4064327ebb6d0faa8552505bf1cc2c8bd473f443
asut0022 <asut0022@student.monash.edu>
2022-09-20 01:34:16 +0000

    Refactored Bulbasaur class to implement the toggleweapon method

--- src/game/pokemons/Bulbasaur.java
+++ src/game/pokemons/Bulbasaur.java
@@ -2,26 +2,30 @@ package game.pokemons;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.Status;
-import game.actions.AttackAction;
-import game.behaviours.AttackBehaviour;
-import game.behaviours.Behaviour;
-import game.behaviours.WanderBehaviour;
+import game.elements.Element;
 import game.time.TimePerception;
 import game.time.TimePerceptionManager;
+import game.weapons.VineWhip;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Bulbasaur extends Pokemon implements TimePerception {
 
+    private BackupWeapons pokemonBackupWeapons;
+
+
     public Bulbasaur() {
         super("Bulbasaur", 'b');
+        this.addCapability(Element.GRASS);
         this.addCapability(Status.CATCHABLE);
+        this.pokemonBackupWeapons = new BackupWeapons(new VineWhip("Vinewhip",'|',30,"whips", 70));
+
         registerInstance();
     }
 
@@ -38,13 +42,24 @@ public class Bulbasaur extends Pokemon implements TimePerception {
             AffectionManager.getInstance().cleanUp(this);
             map.removeActor(this);
         }
+        this.toggleWeapon(this, map);
         return super.playTurn(actions, lastAction, map, display);
     }
 
-    /**
-     * @param isEquipping FIXME: develop a logic to toggle weapon (put a selected weapon to the inventory - used!);
-     */
-    public void toggleWeapon(boolean isEquipping) {
+
+    public void toggleWeapon(Pokemon pokemon, GameMap map) {
+        boolean containsSpecial = this.getInventory().contains(this.pokemonBackupWeapons.getBackupWeapon());
+
+        if (pokemon.hasCapability(Element.GRASS) && map.locationOf(pokemon).getGround().hasCapability(Element.GRASS)){
+            if(!containsSpecial){
+                this.addItemToInventory(this.pokemonBackupWeapons.getBackupWeapon());
+            }
+        }
+        else{
+            if (containsSpecial) {
+                this.removeItemFromInventory(this.pokemonBackupWeapons.getBackupWeapon());
+            }
+        }
     }
 
     @Override
@@ -57,5 +72,9 @@ public class Bulbasaur extends Pokemon implements TimePerception {
         heal(5);
     }
 
+    @Override
+    protected IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(10, "tackle");
+    }
 
 }
