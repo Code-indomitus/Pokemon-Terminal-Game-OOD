63a572a71ab737872d5025e7f33869e009aeb9d0
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-20 11:54:29 +0800

    AttackBehaviour only returns an AttackAction when the other pokemon/actor is not immune and does not have the same elements as the attacking pokemon itself

--- src/game/behaviours/AttackBehaviour.java
+++ src/game/behaviours/AttackBehaviour.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
 import game.actions.AttackAction;
 import game.elements.Element;
 import game.elements.ElementsHelper;
@@ -19,6 +20,12 @@ import game.pokemons.Squirtle;
  */
 public class AttackBehaviour implements Behaviour {
 
+    private Actor attackingActor;
+
+    public AttackBehaviour(Actor attackingActor) {
+        this.attackingActor = attackingActor;
+    }
+
     /**
      *  HINT: develop a logic to check surrounding, check elements, and return an action to attack that opponent.
      */
@@ -27,8 +34,10 @@ public class AttackBehaviour implements Behaviour {
 
         for (Exit exit : map.locationOf(actor).getExits()) {
             Location destination = exit.getDestination();
-            if (destination.containsAnActor()) {
-                return new AttackAction(destination.getActor(),"there");// behaviour will stop here.
+            if (destination.containsAnActor() && !destination.getActor().hasCapability(Status.IMMUNE)) {
+                if (!ElementsHelper.hasAnySimilarElements(destination.getActor(), attackingActor.findCapabilitiesByType(Element.class))){
+                    return new AttackAction(destination.getActor(),"there", map);// behaviour will stop here.
+                }
             }
         }
 
