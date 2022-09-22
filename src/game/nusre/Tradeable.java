aced15a2aa9b3ab20605a12f77d9bc66fd040e23
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-22 22:21:38 +0800

    Java Doc added for Tradealbe.

--- src/game/nusre/Tradeable.java
+++ src/game/nusre/Tradeable.java
@@ -1,5 +1,13 @@
 package game.nusre;
 
+/**
+ * Interface for classes that are tradeable in the game
+ * @author Shyam Kamalesh Borkar
+ */
 public interface Tradeable {
+    /**
+     * method that tells if the class that implements the Tradeable interface is a pokemon
+     * @return if class is a pokemon
+     */
     boolean isPokemon();
 }
