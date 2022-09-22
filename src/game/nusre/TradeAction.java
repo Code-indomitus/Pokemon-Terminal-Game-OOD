6b23ee0cfbc62dfbf8723e845cb9529c86c5c75d
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-22 22:28:33 +0800

    Java Doc added for TradeAction

--- src/game/nusre/TradeAction.java
+++ src/game/nusre/TradeAction.java
@@ -12,15 +12,38 @@ import game.pokemons.Pokemon;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * TradeAction class that allows players to trade items
+ * @author Shyam Kamalesh Borkar
+ */
 public class TradeAction extends Action {
+    /**
+     * the item that is to be traded
+     */
     Tradeable itemToBeTraded;
+
+    /**
+     * number of candies for the transaction to be successful
+     */
     int numberOfCandies;
 
+    /**
+     * TradeAction constructor
+     * @param itemToBeTraded the item that is to be traded
+     * @param numberOfCandies required number of candies for the transaction
+     */
     public TradeAction(Tradeable itemToBeTraded, int numberOfCandies) {
         this.itemToBeTraded = itemToBeTraded;
         this.numberOfCandies = numberOfCandies;
     }
 
+    /**
+     * the execute logic for the TradeAction. If player has insufficient candies transaction
+     * does not take place. If sufficient funds are available transaction takes place.
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (TradeManager.getCandyCount(actor) < numberOfCandies) {
@@ -49,7 +72,11 @@ public class TradeAction extends Action {
         return actor + " trades " + numberOfCandies + " candies for a " + itemToBeTraded + ".";
     }
 
-
+    /**
+     * method that returns string that will be used in the menu desctiption for the player to decide on
+     * @param actor The actor performing the action.
+     * @return menu description string
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " trades " + itemToBeTraded + " with " + numberOfCandies + " candies.";
