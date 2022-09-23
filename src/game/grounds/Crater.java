f5f19d6e4ffeb7a8b1318d3d756eacfdcf2b2649
Your Name Here <eooi0006@student.monash.edu>
2022-09-23 17:31:45 +0800

    Update Java doc for crater

--- src/game/grounds/Crater.java
+++ src/game/grounds/Crater.java
@@ -5,12 +5,22 @@ import game.elements.Element;
 import game.items.Pokefruit;
 import game.pokemons.Charmander;
 
+/**
+ * @author Shyam Kamalesh Borkar
+ * Class for Crater (Spawning Ground)
+ */
 public class Crater extends SpawningGround{
-
+    /**
+     * Crater constructor
+     */
     public Crater() {
         super('O', 0.1, 0.25, 0, Element.FIRE);
     }
 
+    /**
+     * Overridden method to spawn the pokemon, specific to the ground
+     * @param spawnLocation
+     */
     @Override
     public void spawnPokemon(Location spawnLocation) {
         spawnLocation.addActor(new Charmander());
