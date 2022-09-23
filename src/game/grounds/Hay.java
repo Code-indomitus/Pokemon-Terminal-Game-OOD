84385216bb18e11ca91589bd19f202b19308cb1c
Your Name Here <eooi0006@student.monash.edu>
2022-09-23 17:34:35 +0800

    Update Java doc for hay

--- src/game/grounds/Hay.java
+++ src/game/grounds/Hay.java
@@ -3,9 +3,13 @@ package game.grounds;
 import edu.monash.fit2099.engine.positions.Ground;
 import game.elements.Element;
 
+/**
+ * @author Shyam Kamalesh Borkar
+ * Class for Hay (Ground)
+ */
 public class Hay extends Ground {
     /**
-     * Constructor
+     * Hay Constructor
      */
     public Hay() {
         super(',');
