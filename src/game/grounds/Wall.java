715ef688c33a75007e7e4dc29d34d6c0830ad838
Your Name Here <eooi0006@student.monash.edu>
2022-09-23 17:45:12 +0800

    Update Java doc for wall

--- src/game/grounds/Wall.java
+++ src/game/grounds/Wall.java
@@ -3,19 +3,33 @@ package game.grounds;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import game.Status;
-
+/**
+ * @author Shyam Kamalesh Borkar
+ * Class for Wall (Ground)
+ */
 public class Wall extends Ground {
-
+	/**
+	 * Walll constructor
+	 */
 	public Wall() {
 		super('#');
 		this.addCapability(Status.CANNOT_BE_EXPANDED);
 	}
-	
+
+	/**
+	 * Overridden method to not allow actor to enter
+	 * @param actor the Actor to check
+	 * @return false
+	 */
 	@Override
 	public boolean canActorEnter(Actor actor) {
 		return false;
 	}
-	
+
+	/**
+	 * Overriden method to block throwable objects
+	 * @return true
+	 */
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
