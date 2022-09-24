42f94a8db1b5e6baca1e1f5fd049852d0052d8e1
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-24 19:07:11 +0800

    Updated javadoc for AttackAction class

--- src/game/actions/AttackAction.java
+++ src/game/actions/AttackAction.java
@@ -12,9 +12,8 @@ import edu.monash.fit2099.engine.weapons.Weapon;
 /**
  * An Action to attack another Actor.
  * Created by:
- *
  * @author Riordan D. Alfredo
- * Modified by:
+ * Modified by: Arrtish Suthan, Shyam Kamalesh Borkar
  */
 public class AttackAction extends Action {
 
@@ -43,6 +42,16 @@ public class AttackAction extends Action {
         this.direction = direction;
     }
 
+    /**
+     * Class execute method. First, the attacking pokemon's weapons are called from its inventory. Then, a random int value is generated
+     * to determine if the pokemon hit. If the random number is lesser than the weapon's chance to hit, the pokemon hits, else it misses and a string is printed.
+     * When the attacker hits a target, the target pokemon is hurt by the weapon's damage and the target is checked if the pokemon is conscious or not.
+     * If the pokemon is not conscious, the cleanup methods are called to remove the target from thee gamemap, and to drop its respective items.
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return the result of the operation
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
 
@@ -70,6 +79,11 @@ public class AttackAction extends Action {
         return result;
     }
 
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @return a printed string that denotes the attacker attacking a target in a given direction
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " attacks " + target + " at " + direction;
