3bed9e1acc2b8ff7e12b92038a5de002f3fca62b
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-24 18:59:50 +0800

    Updated javadoc for Bubble class

--- src/game/weapons/Bubble.java
+++ src/game/weapons/Bubble.java
@@ -2,6 +2,14 @@ package game.weapons;
 
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 
+/**
+ * Class representing the Bubble weapon item
+ *
+ * Created by:
+ * @author Arrtish Suthan
+ * Modified by:
+ *
+ */
 public class Bubble extends WeaponItem {
 
 
