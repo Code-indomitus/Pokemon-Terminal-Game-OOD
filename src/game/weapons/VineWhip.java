37841c48c94255c30c1dd837e582b77378c32bcc
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-24 18:59:36 +0800

    Updated javadoc for VineWhip class

--- src/game/weapons/VineWhip.java
+++ src/game/weapons/VineWhip.java
@@ -2,6 +2,14 @@ package game.weapons;
 
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 
+/**
+ * Class representing the VineWhip weapon item
+ *
+ * Created by:
+ * @author Arrtish Suthan
+ * Modified by:
+ *
+ */
 public class VineWhip extends WeaponItem {
 
 
