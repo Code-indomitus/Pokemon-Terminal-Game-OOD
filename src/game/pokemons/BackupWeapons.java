6d5547f75be2f9a149106227be32fb6147f49201
Arrtish27 <103437339+Arrtish27@users.noreply.github.com>
2022-09-24 19:03:06 +0800

    Updated javadoc for BackupWeapons class

--- src/game/pokemons/BackupWeapons.java
+++ src/game/pokemons/BackupWeapons.java
@@ -9,7 +9,6 @@ import game.actions.AttackAction;
  * @author Riordan D. Alfredo
  * Modified by:
  *
- * TODO: Use this class to store Pokemon's weapons (special attack) permanently.
  * If a Pokemon needs to use a weapon, put it into that Pokemon's inventory.
  * @see Actor#getWeapon() method.
  * @see AttackAction uses getWeapon() in the execute() method.
