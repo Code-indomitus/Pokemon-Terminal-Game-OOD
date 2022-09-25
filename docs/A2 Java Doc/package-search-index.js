b5dd177baaa99d54a2bb67d6c3dba7a507df5c22
@shyam.borkar108 <glpat-3AhfYh7VxDcgnYQPxTUS>
2022-09-25 12:56:00 +0800

    Java Doc produced/generated for A2 pokemon game code/packages

---
+++ docs/A2 Java Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"game"},{"l":"game.actions"},{"l":"game.behaviours"},{"l":"game.elements"},{"l":"game.grounds"},{"l":"game.items"},{"l":"game.nurse"},{"l":"game.pokemons"},{"l":"game.time"},{"l":"game.weapons"}];updateSearchResults();
