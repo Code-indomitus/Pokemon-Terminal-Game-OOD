726a56cc4975bf0ebef401214cea7a4dda619679
Your Name Here <eooi0006@student.monash.edu>
2022-10-07 20:14:39 +0800

    Added new incubate status for the incubator ground

--- src/game/Status.java
+++ src/game/Status.java
@@ -11,5 +11,7 @@ public enum Status {
     TRADEABLE_CANDY, // used to indicate whether an item in the player's inventory is a candy or not
     CANNOT_ENTER_FLOOR, // used to make sure that actor with this status cannot enter a floor
     CANNOT_BE_EXPANDED, // to identify if the ground can be expanded.
-    FEEDABLE // used to identify if an item is feedable
+    FEEDABLE, // used to identify if an item is feedable
+    INCUBATE // used to identify a ground is capable of incubating Pokemon eggs.
+
 }
