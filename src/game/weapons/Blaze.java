bdf9de5116067eefd04fc0688b038f0abb3dc93f
sbor0018 <sbor0018@student.monash.edu>
2022-10-11 00:59:29 +0800

    Added and implement relevant weapon items for charmeleon and charizard

---
+++ src/game/weapons/Blaze.java
@@ -0,0 +1,17 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+/**
+ * Class representing the Blaze weapon item
+ * Created by:
+ * @author Shyam Kamalesh Borkar
+ */
+public class Blaze extends WeaponItem {
+    /**
+     * Blaze Constructor.
+     */
+    public Blaze() {
+        super("Blaze", ']', 60, "blazes", 90);
+    }
+}
---
+++ src/game/weapons/FireSpin.java
@@ -0,0 +1,19 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+
+/**
+ * Class representing the FireSpin weapon item
+ * Created by:
+ * @author Shyam Kamalesh Borkar
+ */
+public class FireSpin extends WeaponItem {
+    /**
+     * FireSpin Constructor.
+     */
+    public FireSpin() {
+        super("Fire Spin", '[', 70, "blazes", 90);
+    }
+}
+
